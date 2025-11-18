d9540aa9eb38944eb9438b7fe7b4e3c13a916a67
David Ortiz <david.f.ortizg@gmail.com>
2025-11-17 23:22:57 -0500

    Data download file created and dvc backup created

---
+++ src/nombre_paquete/database/data_download.py
@@ -0,0 +1,28 @@
+from kaggle.api.kaggle_api_extended import KaggleApi
+
+
+if __name__ == "__main__":
+    try:
+        api = KaggleApi()
+        api.authenticate()
+        print("Kaggle authenticated")
+
+    except Exception as e:
+        print("Kaggle authentication failed")
+        print("\nError:", e)
+        print("\nMake sure your kaggle.json file is in the correct location.\n")
+
+        print("On Windows:")
+        print(r"    C:\Users\<YOUR_USERNAME>\.kaggle\kaggle.json")
+
+        print("\nOn Linux / macOS:")
+        print("    ~/.kaggle/kaggle.json")
+
+        print("\nIf the folder '.kaggle' does not exist, create it manually and place kaggle.json inside.")
+        print("Also ensure that the file is named exactly 'kaggle.json'.")
+
+    api.dataset_download_files(
+        'grassknoted/asl-alphabet',   
+        path='../../../data/raw/asl-alphabet',
+        unzip=True
+    )
