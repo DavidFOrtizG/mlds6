2d8da90dfc7f29fb01c9a41208cc68e52f9cb5de
David Ortiz <david.f.ortizg@gmail.com>
2025-11-23 20:36:52 -0500

    Add tar file instead of single files to dvc

--- scripts/data_acquisition/data_download.py
+++ scripts/data_acquisition/data_download.py
@@ -1,4 +1,25 @@
+import os
+import tarfile
 from kaggle.api.kaggle_api_extended import KaggleApi
+
+
+def compress_raw_data(raw_path="../../data/raw", compressed_dir="../../data_compressed", tar_name="data_raw.tar"):
+    """
+    Comprime la carpeta 'data/raw/' en un archivo .tar dentro de 'data_compressed/'.
+    """
+    os.makedirs(compressed_dir, exist_ok=True)
+
+    tar_path = os.path.join(compressed_dir, tar_name)
+
+    if os.path.exists(tar_path):
+        os.remove(tar_path)
+
+    print(f"Creando archivo comprimido: {tar_path}")
+
+    with tarfile.open(tar_path, "w") as tar:
+        tar.add(raw_path, arcname=os.path.basename(raw_path))
+
+    print(f"TAR creado correctamente en: {tar_path}")
 
 
 if __name__ == "__main__":
@@ -21,8 +42,15 @@ if __name__ == "__main__":
         print("\nIf the folder '.kaggle' does not exist, create it manually and place kaggle.json inside.")
         print("Also ensure that the file is named exactly 'kaggle.json'.")
 
+
     api.dataset_download_files(
-        'grassknoted/asl-alphabet',   
+        'grassknoted/asl-alphabet',
         path='../../data/raw/asl-alphabet',
         unzip=True
+    )
+
+    compress_raw_data(
+        raw_path='../../data/raw',
+        compressed_dir='../../data_compressed',
+        tar_name='data_raw.tar'
     )
